b26a45261e6c6b35b4eef08564e2cb360a9abd81
Rice <kafin@naver.com>
2024-01-09 04:34:17 +0900

    MemberApp, MemberServiceTest add

---
+++ src/main/java/rice/spring/MemberApp.java
@@ -0,0 +1,18 @@
+package rice.spring;
+
+import rice.spring.member.Grade;
+import rice.spring.member.Member;
+import rice.spring.member.MemberService;
+import rice.spring.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+         memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new Member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
